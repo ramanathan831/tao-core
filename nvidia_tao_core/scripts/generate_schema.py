f0885757171b86aeec1702d34c8aebff125647f6
Ramanathan Arunachalam <rarunachalam@nvidia.com>
2024-11-13 10:55:58 -0800

    [Bugfix] Bevfusion - Support specific dataclass return for an action

--- nvidia_tao_core/scripts/generate_schema.py
+++ nvidia_tao_core/scripts/generate_schema.py
@@ -17,9 +17,12 @@
 from nvidia_tao_core.api_utils import dataclass2json_converter
 
 
-def generate_schema(neural_network_name):
+def generate_schema(neural_network_name, action=""):
     """Generates JSON schema for network"""
     imported_module = dataclass2json_converter.import_module_from_path(f"nvidia_tao_core.config.{neural_network_name}.default_config")
-    expConfig = imported_module.ExperimentConfig()
+    if neural_network_name == "bevfusion" and action == "dataset_convert":
+        expConfig = imported_module.BEVFusionDataConvertExpConfig()
+    else:
+        expConfig = imported_module.ExperimentConfig()
     json_with_meta_config = dataclass2json_converter.dataclass_to_json(expConfig)
     return dataclass2json_converter.create_json_schema(json_with_meta_config)
