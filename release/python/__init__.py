a84f5399ed0ad0c0b0d431fc581ddba61cfeba75
Adrian Tran <hqtran-nv@users.noreply.github.com>
2026-05-22 09:42:15 +0700

    [TAO-0000][Chore] OSRB Compliance

--- release/python/__init__.py
+++ release/python/__init__.py
@@ -1,6 +1,4 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-# Copyright (c) 2022-2023, NVIDIA CORPORATION.  All rights reserved.
-
 """Packaging modules for TAO Toolkit."""
